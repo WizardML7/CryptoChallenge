ee8a5af5018dc0b8d125d4db79145d24be610afc
WizardML7 <dml3483@g.rit.edu>
2023-12-27 13:55:15 -0500

    Got the message from the LSB!

--- answer.py
+++ answer.py
@@ -1,4 +1,6 @@
 import requests
+import zlib
+import base64
 
 # Global values
 base = "http://crypto.praetorian.com/{}"
@@ -53,10 +55,73 @@ def caesarCipher(ciphertext):
         
     return decrypted_text
 
+def extract_pixel_data(base64_encoded_png):
+    try:
+        # Ensure correct Base64 padding
+        padding = len(base64_encoded_png) % 4
+        base64_encoded_png += b'=' * (4 - padding)
+
+        # print("LOOK HERE \n")
+        # print(base64_encoded_png)
+
+        # Decode Base64
+        binary_png = base64.b64decode(base64_encoded_png)
+
+        # Find the start and end positions of IDAT and IEND chunks
+        idat_start = binary_png.find(b'\x49\x44\x41\x54')  # IDAT chunk
+        iend_start = binary_png.find(b'\x49\x45\x4E\x44')  # IEND chunk
+
+        if idat_start == -1 or iend_start == -1:
+            print("IDAT or IEND chunk not found.")
+            return None
+
+        # Extract data between IDAT and IEND
+        compressed_data = binary_png[idat_start + 4:iend_start]
+
+        # Decompress the data
+        decompressed_data = zlib.decompress(compressed_data)
+
+        # print("THIS IS THE DECOMPRESSED DATA: \n")
+
+        # print(decompressed_data)
+
+        return decompressed_data
+    except (base64.binascii.Error, zlib.error) as e:
+        print("Error:", e)
+        return None
+
+def extract_message_from_pixel_data(pixel_data):
+    try:
+        message_bits = []
+
+        # Iterate through each byte in the pixel data
+        for byte in pixel_data:
+            # Extract all 8 bits from each byte
+            for i in range(7, -1, -1):
+                # Extract the i-th bit and append to the message_bits list
+                message_bits.append((byte >> i) & 1)
+
+        # Convert the list of bits to bytes
+        message_bytes = bytearray()
+        for i in range(0, len(message_bits), 8):
+            byte = 0
+            for j in range(8):
+                byte = (byte << 1) | message_bits[i + j]
+            message_bytes.append(byte)
+
+        # Convert the bytes to a string (assuming the message is ASCII characters)
+        message = message_bytes.decode('ascii', errors='replace')
+
+        print(message)
+
+        return message
+    except UnicodeDecodeError as e:
+        print("Error decoding message:", e)
+        return None
 
 hashes = {}
 
-for i in range(0, 4):
+for i in range(3, 4):
     level = i
     data = fetch(level)
 
@@ -73,7 +138,12 @@ for i in range(0, 4):
         h = solve(level, guess)
         if 'hash' in h: hashes[level] = h['hash']
     elif level == 3:
-        guess = input("Input guess here: ")
+        guess = data['challenge']
+        guess = guess[22:]
+        guess = guess.encode("utf-8")
+        guess = extract_pixel_data(guess)
+        guess = extract_message_from_pixel_data(guess)
+        guess = input("Give the guess here: ")
         h = solve(level, guess)
         if 'hash' in h: hashes[level] = h['hash']
     else:
